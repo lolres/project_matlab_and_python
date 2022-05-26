53669c917032ea15559713aef44bdb5b7e1e9a5f
lolres <lolres007@mail.ru>
2022-05-26 18:52:17 +0300

    обновление, 2 задание матлаб

--- Задание 2/Matlab/zadanie_2.m
+++ Задание 2/Matlab/zadanie_2.m
@@ -4,16 +4,22 @@ syms x a b
 
 filename = fopen('integrals_matlab.txt','w'); % открываем файл на запись
 fprintf(filename,'%9s%11s%11s%13s%13s%24s\n','Nomer','lim1', 'lim2', 'a', 'b', 'result');
-a = randi([1, 100]); % рандомное число
-b = randi([1, 100]); % рандомное число
-integ = subs(int(1 / x / sqrt(a*x+b), x), a, b); % вычисляем неопределенный интеграл
+% печатаем шапку таблицы
+
+a = 54; % задаем константу
+b = 77; % задаем константу
+lim1 = 2; % задаем предел интегрирования для отчета
+lim2 = 10; % задаем предел интегрирования для отчета
+integ = int(1 / x / sqrt(a*x+b), x); % вычисляем неопределенный интеграл
+result = subs(integ, x, lim2) - subs(integ, x, lim1); % результат для отчета
+fprintf(filename,'%9d%11d%11d%13d%13d%24.6f\n', 1, lim1, lim2, a, b, result); % выводим переменные
 tic; % начало отсчета времени выполнения алгоритма
-for i = 1:100
+for i = 2:100
     lim1 = randi([1, 100]); % рандомное число
     lim2 = randi([1, 100]); % рандомное число
     result = subs(integ, x, lim2) - subs(integ, x, lim1); % подставляем пределы интегрирования
     fprintf(filename,'%9d%11d%11d%13d%13d%24.6f\n', i, lim1, lim2, a, b, result); % выводим переменные
 end
 time_end = toc; % конец отсчета времени выполнения алгоритма
-fprintf(filename, "Вычислено за " + num2str(time_end) + " секунд");
-fclose(filename); % закрываем число
+fprintf(filename, "Вычислено за " + num2str(time_end) + " секунд"); % печатаем время выполнения
+fclose(filename); % закрываем файл
