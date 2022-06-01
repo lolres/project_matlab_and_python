07f392b4f09ba709fb8d5821c111ae0498383fe6
Artyom Zabroda <artyomzabroda@gmail.com>
2022-06-01 18:22:11 +0300

    Обновил задание 2 python

--- Задание 2/Python/TaskSympy.py
+++ Задание 2/Python/TaskSympy.py
@@ -3,24 +3,13 @@ from sympy.abc import a, x, y
 import random
 from time import *
 
-def timer_func(func):
-    def wrap_func(*args, **kwargs):
-        t1 = time() # время на начало выполнения функции
-        result = func(*args, **kwargs)
-        t2 = time() # время на конец выполнения функции
-        with open("time.txt", mode='a') as f:
-            f.write(f"{(t2-t1):.4f}s\n")
-        print(f"{(t2-t1):.4f}s")
-        return result
-    return wrap_func
-
-@timer_func
 def rand_integral(x, predel_1, predel_2):
     result = (integ.subs(x, predel_2) - integ.subs(x, predel_1)).evalf() # результат вычисляем 
     print(result)
     return (str(result) + '\n') # возвращаем результат в виде строки
 
 if __name__ == '__main__':
+    t1 = time()
     x = symbols("x") 
     s = str()
     a = 54
@@ -37,5 +26,7 @@ if __name__ == '__main__':
         s += rand_integral(x, predel_1, predel_2)
     with open("results.txt", mode="w") as f:
         f.write(s) # записываем строку в файл
+    t2 = time()
+    print(f"{(t2-t1):.4f}s")
 
 
